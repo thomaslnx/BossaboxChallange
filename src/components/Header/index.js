e20bb2c7c6cabb306d7d413d54ba8f3737b0e62a
thomaslnx <marcos.msilva10@gmail.com>
2019-12-03 17:22:16 -0300

    coding styles

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+export default function Header() {
+  return (
+    <>
+      <Title>
+        <p className="title">VUTTR</p>
+        <p className="titleDescription">Very Useful Tools to Remember</p>
+      </Title>
+    </>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Title = styled.p`
+  font-family: 'Source Sans Pro', sans-serif;
+  font-size: 42px;
+  color: #170c3a;
+  display: flex;
+  flex-direction: column;
+  line-height: 0;
+
+  .title {
+    font-weight: 600;
+  }
+
+  .titleDescription {
+    font-size: 26px;
+  }
+`;
